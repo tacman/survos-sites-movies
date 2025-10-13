8a15eb75dc0ba7ee4f962497b24624dffc993a93
Tac Tacelosky <tacman@gmail.com>
2025-10-13 12:58:48 -0400

    initial version

--- importmap.php
+++ importmap.php
@@ -25,4 +25,111 @@ return [
     '@hotwired/turbo' => [
         'version' => '7.3.0',
     ],
+    'twig' => [
+        'version' => '1.17.1',
+    ],
+    'locutus/php/strings/sprintf' => [
+        'version' => '2.0.16',
+    ],
+    'locutus/php/strings/vsprintf' => [
+        'version' => '2.0.16',
+    ],
+    'locutus/php/math/round' => [
+        'version' => '2.0.16',
+    ],
+    'locutus/php/math/max' => [
+        'version' => '2.0.16',
+    ],
+    'locutus/php/math/min' => [
+        'version' => '2.0.16',
+    ],
+    'locutus/php/strings/strip_tags' => [
+        'version' => '2.0.16',
+    ],
+    'locutus/php/datetime/strtotime' => [
+        'version' => '2.0.16',
+    ],
+    'locutus/php/datetime/date' => [
+        'version' => '2.0.16',
+    ],
+    'locutus/php/var/boolval' => [
+        'version' => '2.0.16',
+    ],
+    'stimulus-attributes' => [
+        'version' => '1.0.2',
+    ],
+    'escape-html' => [
+        'version' => '1.0.3',
+    ],
+    'fos-routing' => [
+        'version' => '0.0.6',
+    ],
+    'flag-icons' => [
+        'version' => '7.5.0',
+    ],
+    'flag-icons/css/flag-icons.min.css' => [
+        'version' => '7.5.0',
+        'type' => 'css',
+    ],
+    'instantsearch.js' => [
+        'version' => '4.80.0',
+    ],
+    '@algolia/events' => [
+        'version' => '4.0.1',
+    ],
+    'algoliasearch-helper' => [
+        'version' => '3.26.0',
+    ],
+    'qs' => [
+        'version' => '6.9.7',
+    ],
+    'algoliasearch-helper/types/algoliasearch.js' => [
+        'version' => '3.26.0',
+    ],
+    'instantsearch.js/es/widgets' => [
+        'version' => '4.80.0',
+    ],
+    'instantsearch-ui-components' => [
+        'version' => '0.11.2',
+    ],
+    'preact' => [
+        'version' => '10.27.1',
+    ],
+    'hogan.js' => [
+        'version' => '3.0.2',
+    ],
+    'htm/preact' => [
+        'version' => '3.1.1',
+    ],
+    'preact/hooks' => [
+        'version' => '10.27.1',
+    ],
+    '@babel/runtime/helpers/extends' => [
+        'version' => '7.27.6',
+    ],
+    '@babel/runtime/helpers/defineProperty' => [
+        'version' => '7.27.6',
+    ],
+    '@babel/runtime/helpers/objectWithoutProperties' => [
+        'version' => '7.27.6',
+    ],
+    'htm' => [
+        'version' => '3.1.1',
+    ],
+    '@meilisearch/instant-meilisearch' => [
+        'version' => '0.27.0',
+    ],
+    'meilisearch' => [
+        'version' => '0.53.0',
+    ],
+    '@stimulus-components/dialog' => [
+        'version' => '1.0.1',
+    ],
+    'pretty-print-json' => [
+        'version' => '3.0.5',
+    ],
+    'pretty-print-json/dist/css/pretty-print-json.min.css' => [
+        'version' => '3.0.5',
+        'type' => 'css',
+    ],
 ];
